69a8cf839318799c3ca98314e9b1a2270ad39b60
sskim91 <tjdtjq91@gmail.com>
2025-02-28 17:29:23 +0900

    Initial commit

---
+++ src/main/java/com/barogo/app/BarogoTasksApplication.java
@@ -0,0 +1,13 @@
+package com.barogo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarogoTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarogoTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/barogo/app/BarogoTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barogo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarogoTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
